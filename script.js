ae372efc4e576c31980c24101ceebafed7db1b41
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 17:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+let url=window.navigator;
+let body=document.getElementsByTagName("body")[0];
+body.innerHTML=`<div id="browser-info">You are using ${url.appName} version ${url.appVersion}</div>`;
